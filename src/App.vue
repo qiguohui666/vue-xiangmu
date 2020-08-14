7b058893a84507de96289ca2a3320a401e4e302b
qiguohui <891864396@qq.com>
2020-08-14 21:08:53 +0800

    login组件

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+      <LOGIN />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import LOGIN from './pages/login'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    LOGIN
   }
 }
 </script>
---
+++ src/pages/login.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        login ZUIJAN 
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
